bd819431db159ccd1de049183311cfb93c762f0b
Rodrigo Herrera <rodrigoherreraitl@gmail.com>
2025-01-28 22:48:54 -0600

    cast

--- src/components/registerform.jsx
+++ src/components/registerform.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate , Link } from "react-router-dom";
-import './registForm.css'
+import './registform.css'
 
 const FormRegistrationApp = () => {
 
