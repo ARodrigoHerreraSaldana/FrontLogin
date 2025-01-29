9cc95693f9c2a499b3269fd4ee387b85b87d4850
Rodrigo Herrera <rodrigoherreraitl@gmail.com>
2025-01-28 22:17:16 -0600

    for vite

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import builtins from 'rollup-plugin-node-builtins';
-import globals from 'rollup-plugin-node-globals';
-
 export default defineConfig({
   build: {
-    rollupOptions: {
-      external: ['react'], // Add other third-party packages here
-    },
-  },
-  plugins: [
-    react(),
-    builtins(),
-    globals(),
-  ],
-  resolve: {
-    alias: {
-      'crypto-js': 'crypto-js',
-    },
-    define: {
-      _global: ({})
   },
   optimizeDeps: {
     include: ['@emotion/styled'],
   },
-  },
+  plugins: [
+    react(),
+  ],
 });
